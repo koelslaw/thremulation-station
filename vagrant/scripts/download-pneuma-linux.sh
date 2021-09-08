c6873063812d6125e1014280672399c71066b519
webhead404 <kvvincentvalentine@gmail.com>
2021-09-07 21:04:06 -0500

    Pushed service file entry to script

--- vagrant/scripts/download-pneuma-linux.sh
+++ vagrant/scripts/download-pneuma-linux.sh
@@ -2,6 +2,7 @@
 
 PNEUMA_URL="https://s3.amazonaws.com/operator.payloads.open/payloads/pneuma/pneuma-linux"
 INSTALL_DIR="/opt/pneuma"
+SCRIPTS_DIR="/vagrant"
 
 # Stage Pneuma download
 
@@ -10,6 +11,21 @@ curl $PNEUMA_URL -o pneuma-agent
 mkdir $INSTALL_DIR
 cp pneuma-agent $INSTALL_DIR
 chmod +x $INSTALL_DIR/pneuma-agent
+
+
+
+echo "[Unit]
+Description=Pneuma-Agent
+
+[Service]
+ExecStart=/opt/pneuma/pneuma-agent -address "192.168.33.13:2323" -contact "tcp" -name "pneuma-centos7" -range "thremulation"
+Restart=on-failure
+StartLimitInterval=600
+RestartSec=15
+StartLimitBurst=16
+
+[Install]
+WantedBy=multi-user.target" > pneuma-agent.service
 cp pneuma-agent.service /etc/systemd/system
 
 systemctl enable pneuma-agent
